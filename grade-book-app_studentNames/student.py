821cf0e0f62dd758fc134d394f463d3b0fc57580
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-07-23 06:12:16 -0700

    Update student.py

---
+++ grade-book-app_studentNames/student.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, email, names):
+        self.email = email
+        self.names = names
+        self.courses_registered = []
+        self.GPA = 0.0
+    
+    def calculate_GPA(self):
+        if not self.courses_registered:
+            self.GPA = 0.0
+        else:
+            total_credits = sum(course['credits'] for course in self.courses_registered)
+            total_points = sum(course['grade'] * course['credits'] for course in self.courses_registered)
+            self.GPA = total_points / total_credits if total_credits != 0 else 0.0
+    
+    def register_for_course(self, course, grade):
+        self.courses_registered.append({'course': course, 'grade': grade})
+        self.calculate_GPA()
