a59055a76452d508e3ed62ea33bd3f1e8a0bc8f3
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-07-29 12:55:59 +0200

    Update gradebook.py

--- grade-book-app_studentNames/gradebook.py
+++ grade-book-app_studentNames/gradebook.py
@@ -20,7 +20,6 @@ class GradeBook:
         course = next((c for c in self.course_list if c.name == course_name), None)
         if student and course:
             student.register_for_course(course, grade)
-            return True
         return False
 
     def calculate_ranking(self):
